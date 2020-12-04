c2cfc66589a89ff6e9e3cc813c16d528631a0fcc
Oliwia <oliwia.second@gmail.com>
2020-12-05 00:00:19 +0100

    some idea things

--- StatsWorld/CallAPI.cs
+++ StatsWorld/CallAPI.cs
@@ -1,4 +1,26 @@
-$HEADER$namespace $NAMESPACE$
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+
+using System.Threading.Tasks;
+
+namespace StatsWorld
 {
-  public class $CLASS$ {$END$}
+    public class CallAPI
+    {
+        public async Task<List<StatsFromCountryModel>> GetProductAsync(string path)
+        {
+            HttpClient client = new HttpClient();
+            var product = new List<StatsFromCountryModel>();
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await response.Content.ReadAsAsync<List<StatsFromCountryModel>>();
+            }
+            return product;
+            
+        }
+
+    }
 }
--- StatsWorld/GetStatsScrapper.cs
+++ StatsWorld/GetStatsScrapper.cs
@@ -1,4 +1,25 @@
-$HEADER$namespace $NAMESPACE$
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StatsWorld
 {
-  public class $CLASS$ {$END$}
+    public class GetStatsScrapper
+    {
+        public async Task<List<List<StatsFromCountryModel>>> GetStatics(){
+        List <ModelCountry> countries = await new StatsWorldScrapper().GetCountriesNames();
+        List<StatsFromCountryModel> oneCountry = new List<StatsFromCountryModel>(); 
+        List<List<StatsFromCountryModel>> allCountries = new List<List<StatsFromCountryModel>>();
+        foreach (var country in countries)
+        {
+            String apiForm;
+            apiForm = $"https://api.covid19api.com/total/dayone/country/{country.Country}";
+            oneCountry = await new CallAPI().GetProductAsync(apiForm);
+            allCountries.Add(oneCountry);
+        }
+
+        return allCountries;
+
+        }
+    }
 }
