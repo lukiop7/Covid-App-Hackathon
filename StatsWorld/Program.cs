7d3f4a7480531b1ba28e3920ba46c3ab73399c20
Oliwia <oliwia.second@gmail.com>
2020-12-04 21:47:39 +0100

    added stats world

---
+++ StatsWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StatsWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
