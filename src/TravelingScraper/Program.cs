ef32daa25d41b69daf308d3761229526411491ac
Bartek Piura <Bartek9805@hotmail.com>
2020-12-04 19:15:22 +0100

    base scarping

---
+++ src/TravelingScraper/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TravelingScraper
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(await new TravelingScraper().GetTravelingInformation());
+        }
+    }
+}
---
+++ src/TravelingScraper/TravelingScraper.cs
@@ -0,0 +1,34 @@
+﻿using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TravelingScraper
+{
+    public class TravelingScraper
+    {
+        private string siteUrl = "https://www.gov.pl/web/dyplomacja/koronawirus-podroze-za-granice";
+
+        public async Task<string> GetTravelingInformation()
+        {
+            ScrapingBrowser browser = new ScrapingBrowser();
+            var page = await browser.NavigateToPageAsync(new Uri(siteUrl));
+            var list = page.Html.CssSelect("Table");
+
+            foreach(var element in list)
+            {
+                var tittle = element.PreviousSibling.PreviousSibling.InnerText;
+                Console.WriteLine(tittle);
+            }
+
+            return "";
+
+        }
+    }
+
+    public class TravelingInformation
+    {
+
+    }
+}
