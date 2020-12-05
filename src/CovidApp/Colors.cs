d1aa14f0b91340be2a4d3ea6a6cb3ae8d3b3e0d5
Filip Matynia <koncepcjonista37@outlook.com>
2020-12-05 02:11:47 +0100

    Colorki znowu

--- src/CovidApp/Colors.cs
+++ src/CovidApp/Colors.cs
@@ -15,8 +15,35 @@ namespace CovidApp
 
             string[] s = new string[quantity];
 
-            for(int i = 0; i < quantity; i++)
-            s[i] = ColorUtil.ColorHexString((byte)r.Next(40, 250), (byte)r.Next(40, 250), (byte)r.Next(40, 250));
+            for (int i = 0; i < quantity; i++)
+                s[i] = ColorUtil.ColorHexString((byte)r.Next(40, 250), (byte)r.Next(40, 250), (byte)r.Next(40, 250));
+
+            return s;
+        }
+        public static string[] RandomColors(int quantity, int brightness)
+        {
+            Random r = new Random();
+            string[] s = new string[quantity];
+
+            Vector3 color = new Vector3(brightness, brightness, brightness);
+            float lenght = color.Length();
+            //Vector3 color2 = Vector3.Normalize(new Vector3(r.Next(255), r.Next(255), r.Next(255))); 
+
+            for (int i = 0; i < quantity; i++)
+            {
+                float x = (float)r.NextDouble() * 1f - 0.5f;
+                float y = (float)r.NextDouble() * 1f - 0.5f;
+                float z = (float)r.NextDouble() * 1f - 0.5f;
+                Vector3 tmp = Vector3.Transform(color, Matrix4x4.CreateRotationX(x) * Matrix4x4.CreateRotationY(y) * Matrix4x4.CreateRotationZ(z));
+
+                s[i] = ColorUtil.ColorHexString((byte)tmp.X, (byte)tmp.Y, (byte)tmp.Z);
+
+                if (tmp.Length() < lenght) {
+                    Console.WriteLine(tmp.Length()+"  >  "+ lenght);
+                    Console.WriteLine(((byte)tmp.X, (byte)tmp.Y, (byte)tmp.Z) + "");
+                    Console.WriteLine(x+"  "+y+"  "+z);
+                }
+            }
 
             return s;
         }
